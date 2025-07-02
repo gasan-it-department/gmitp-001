9482d8af7e096128817dec0aeb0aa6a5ec748faa
Rizza Nobleza <rizzabhb24024@gmail.com>
2025-07-02 16:56:19 +0800

    TESTING ULIT

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,23 +6,6 @@
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>v
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-
-        var asdasdasdasdasd = "asd";var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        var asdasdasdasdasd = "asd";
-        
             (function() {
                 const appearance = '{{ $appearance ?? "system" }}';
 
@@ -52,6 +35,7 @@
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="{{ asset('assets/group.png') }}">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
+    return Inertia::render('home');
 });
 
 require __DIR__.'/settings.php';
