85f2b839748aeb0ec62f423fd9ca170c5378a63a
Mr.Kedge <101467604+MrKedge@users.noreply.github.com>
2025-07-15 13:17:49 +0800

    added the Business permit page

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/news-and-events', fn() => Inertia::render('Public/NewsAndEventsPage
 Route::get('/transparency', fn() => Inertia::render('Public/TransparencyPage'))->name('transparencyPage');
 Route::get('/contact-us', fn() => Inertia::render('Public/ContactUsPage'))->name('contactUsPage');
 // Route::get('/login', fn() => Inertia::render('Auth/Login'))->name('login');
+Route::get('/business-permit', fn() => Inertia::render('Public/ServiceBusinessPermitPage'))->name('businessPermit');
 
 
 require __DIR__ . '/auth.php';
