8a4367211ef4d5b7c5311f71bd48e4a5be224887
Mr.Kedge <101467604+MrKedge@users.noreply.github.com>
2025-12-01 15:40:07 +0800

    added the api for communityreport for getting the list

--- resources/js/Core/Api/CommunityReport/CommunityReportApi.ts
+++ resources/js/Core/Api/CommunityReport/CommunityReportApi.ts
@@ -41,7 +41,7 @@ export const CommunityReportApi = {
     },
 
     async getCommunityReport(municipalSlug: string) {
-        const { url, method, } = CommunityReport.CommunityReportController.show();
+        const { url, method, } = CommunityReport.CommunityReportController.fetch();
 
         const { data } = await axios({
             url,
