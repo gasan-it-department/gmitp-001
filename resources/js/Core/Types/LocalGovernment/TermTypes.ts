7db17cc6675e1695dacb9a7b5951590e2791290a
Mr.Kedge <101467604+MrKedge@users.noreply.github.com>
2026-02-05 08:24:25 +0800

    added term types

---
+++ resources/js/Core/Types/LocalGovernment/TermTypes.ts
@@ -0,0 +1,15 @@
+interface TermFormData {
+    statutory_start: string;
+    statutory_end: string;
+    name: string;
+    is_current: boolean;
+}
+
+interface TermData {
+    id: string;
+    statutory_start: string;
+    statutory_end: string;
+    name: string;
+    is_active: boolean;
+
+}
