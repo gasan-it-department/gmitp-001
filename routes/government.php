fc51bb7ef07d02f6a327be7383ede0856e055d65
Harvey Orencio <harveyorencio@gmail.com>
2026-02-12 15:35:33 +0800

    feat: Enhance term management with new controllers, resources, and UI components for official appointments

--- routes/government.php
+++ routes/government.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Core\Government\UseCase\SearchOfficialsUseCase;
+use App\External\Api\Controllers\Government\Official\SearchOfficialsController;
 use App\External\Api\Controllers\Government\Terms\CreateTermController;
 use App\External\Api\Controllers\Government\Terms\UpdateTermController;
 use App\External\Web\Controllers\LocalGovernment\Admin\ListTermController;
+use App\External\Web\Controllers\LocalGovernment\Admin\ShowTermController;
 use App\External\Web\Controllers\LocalGovernment\Admin\ShowAppointOfficialController;
 
 Route::prefix('{municipality}/government')
@@ -10,29 +13,33 @@ Route::prefix('{municipality}/government')
     ->middleware(['municipalityContext', 'admin'])
     ->group(function () {
 
+        //terms
         Route::prefix('terms')
             ->name('terms.')
             ->group(function () {
 
-                Route::get('/', ListTermController::class)->name('page');
+            Route::get('/', ListTermController::class)->name('page');
 
-            });
+            Route::get('/{termId}', ShowTermController::class)->name('details');
 
+
+        });
+        //appoint official
         Route::prefix('official-terms')
-            ->name('officials.terms')
+            ->name('officials.terms.')
             ->group(function () {
 
-                Route::get('/{termId}', ShowAppointOfficialController::class)->name('page');
+            Route::get('/appoint-officials/{termId}', ShowAppointOfficialController::class)->name('page');
 
-            });
-
+        });
+        //official
         Route::prefix('officials')
             ->name('officials.')
             ->group(function () {
 
-                // Route::get('/', )->name('page');
-        
-            });
+            // Route::get('/', )->name('page');
+    
+        });
 
 
 
@@ -59,6 +66,8 @@ Route::prefix('api/government')
             ->group(function () {
 
                 Route::post('/')->name('store');
+
+                Route::get('/search-official', SearchOfficialsController::class)->name('search');
 
             });
 
