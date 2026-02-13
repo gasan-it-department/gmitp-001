7c49f07df84a1a310c531d5ac4ef8b01a37c23df
RIXSA <rizzabhb24024@gmail.com>
2026-02-13 10:40:21 +0800

    UPDATEEE

--- resources/js/layouts/Public/Components/navigationItems.ts
+++ resources/js/layouts/Public/Components/navigationItems.ts
@@ -7,6 +7,7 @@ export type NavigationItem = {
   route: ((args: { municipality: string | number }) => { url: string; method: string }) | string;
   icon?: LucideIcon;
   isActive?: boolean;
+  id: string
   children?: {
     label: string;
     href: string;
@@ -18,25 +19,30 @@ export const useNavigation = (): NavigationItem[] => [
     title: "Home",
     route: home,
     icon: House,
+    id: "home"
   },
   {
     title: "Travel",
     route: travel,
     icon: MapPinned,
+    id: "travel"
   },
   {
     title: "Government",
     route: government,
     icon: Landmark,
+    id: "government"
   },
   {
     title: "Transparency",
     route: transparency.index,
     icon: ShieldCheck,
+    id: "transparency"
   },
   {
     title: "Executive Orders",
     route: executiveOrders,
     icon: Folders,
+    id: "executive-orders"
   },
 ];
