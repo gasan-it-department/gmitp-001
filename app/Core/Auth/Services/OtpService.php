89e05246a019f9e99030be0e0c312fa109922b55
Harvey Orencio <harveyorencio.dev@gmail.com>
2026-08-07 15:19:38 +0800

    uncomment otp service line 72

--- app/Core/Auth/Services/OtpService.php
+++ app/Core/Auth/Services/OtpService.php
@@ -69,7 +69,7 @@ class OtpService
 
         RateLimiter::hit($key, self::THROTTLE_SECONDS);
 
-        //$this->sendOtpSms($phoneNumber, $code, $purpose);
+        $this->sendOtpSms($phoneNumber, $code, $purpose);
 
         return $code;
 
